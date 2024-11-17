864e8841944e1ae41bbf74c2fc054debb601d631
nemorsell <dmitrijborisuk21@gmail.com>
2024-11-17 19:40:14 +0300

    qwe

--- server.js
+++ server.js
@@ -4,18 +4,15 @@ const path = require('path');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
-
+const sequelize = require('./config/db.config');
+const viewRoutes = require('./routes/index');
+const apiRoutes = require('./api/users');
 
 const app = express();
  
-
-
 app.use(express.static(path.join(__dirname, 'public')));
-// Parse application/x-www-form-urlencoded 
 app.use(bodyParser.urlencoded({ extended: true}));
-
-
-
+app.use(express.json()); 
 
 app.engine('hbs', exphbs({
      extname: 'hbs', 
@@ -25,63 +22,6 @@ app.engine('hbs', exphbs({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
-
-
-
-app.get('/', (req, res) => {
-  res.render('home', {
-    title: 'Home Page', message: 'My first Node.js Express HBS app' });
-});
-
-
-
-
-app.get('/login', (req, res) => {
-  res.render('login', { title: 'Login Page' });
-});
-
-app.get('/404', (req, res) => {
-  res.render('404', { title: 'not found' });  
-});
-
-
-app.get('/registration', (req, res) => {
-  res.render('registration', { title: 'Registration Page' });
-}); 
-
-app.post('/registration', (req, res) => { 
-    const { username, password, email, birthdate, photo } = req.body; 
-    if (username && password && email) { 
-        console.log(`User registered successfully! Username: ${username}`);
-        return res.redirect('/');
-    }
-    else { 
-        return res.status(400).send('Registration failed. Missing required fields.'); 
-    }
-});
-
-
-
-
-
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
-
-
-app.use((req, res, next) => {
-  res.status(404).render('404', { title: '404 - Page Not Found' });
-});
-
-
-
-
-
-const sequelize = require('./config/db.config');
-
-
 sequelize.sync() 
   .then(() => { 
     console.log('Database & tables created!'); 
@@ -89,3 +29,15 @@ sequelize.sync()
   .catch((err) => { 
     console.error('Error creating database:', err); 
   });
+
+app.use('/', viewRoutes); 
+app.use('/api', apiRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).render('404', { title: '404 - Page Not Found' });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
